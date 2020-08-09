37b51ab9f8920594265a3a48d4e54858a81b5f05
yeonhodev <yeonho.dev@gmail.com>
2020-08-08 23:56:11 -0400

    slicing

---
+++ slicing.py
@@ -0,0 +1,10 @@
+# Python 문자열의 인덱스
+#  P  y  t  h  o  n
+#  0  1  2  3  4  5
+# -6 -5 -4 -3 -2 -1
+
+word = "python"
+print(word[0:6])
+print(word[0:])
+print(word[:6]) # 5번째 인덱스에 해당하는 문자(n)까지 표시된다. 
+print(word[:-1]) # -2번째 인덱스에 해당하는 문자(o)까지 표시된다. 
