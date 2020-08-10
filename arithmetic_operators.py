8a143d2ce80382706653a1f0ade7b52cb75f2dab
yeonhodev <yeonho.dev@gmail.com>
2020-08-09 23:51:27 -0400

    arithmetic operators

---
+++ arithmetic_operators.py
@@ -0,0 +1,8 @@
+# Arithmetic Operators
+print(1 + 2) # 3
+print(3 - 4) # -1
+print(5 * 6) # 30
+print(2 ** 8) # 2의 8승. pow(2, 8)와 동일함
+print(5 / 3) # 1.6666666666666667
+print(5 // 3) # 나눗셈 결과의 몫. 1
+print(5 % 3) # 나눗셈 결과의 나머지. 2
