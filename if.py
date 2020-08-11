3e26beec9dfff453d788a5873a9abee4a96f357b
yeonhodev <yeonho.dev@gmail.com>
2020-08-10 23:37:32 -0400

    Use if statement to determine if it's overbought or oversold

---
+++ if.py
@@ -0,0 +1,10 @@
+# RSI(Relative Strength Index)
+rsi = 88
+if rsi > 70:
+  print("RSI", rsi, "means overbought.")
+elif rsi < 30:
+  print("RSI", rsi, "means oversold.")
+else:
+  print("...")
+
+# RSI 88 means overbought.
