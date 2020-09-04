18dac2f9f72becf493e3f995b2c9107bd169c1f2
yeonhodev <yeonho.dev@gmail.com>
2020-09-03 23:58:18 -0400

    Added import ~ as ~

--- import_example.py
+++ import_example.py
@@ -23,3 +23,16 @@ print(calendar.month(2020, 1)) # 모듈명(calendar) 생략 불가
 
 from calendar import month
 print(month(2020, 2)) # 모듈명(calendar) 생략 가능
+
+# import ~ as ~ 
+# as 예약어를 사용하면 이름이 긴 모듈명을 프로그래머가 원하는 별칭(alias)으로 줄여서 사용할 수 있다. 
+
+# import 이름이_긴_모듈명 as 별칭
+# from ~ import ~ as 별칭
+
+import datetime
+print(datetime.datetime.now()) # 별칭을 사용하지 않은경우
+
+# datetime 모듈의 datetime 타입을 dt라는 별칭으로 지정하면, datetime.datetime.now()대신 dt.now()처럼 코드 길이를 짧게 줄일 수 있다. 
+from datetime import datetime as dt
+print(dt.now()) # 별칭(dt)를 사용한 경우
