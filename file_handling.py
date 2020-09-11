37e0c26c86a81885485c6759645694093d633787
yeonhodev <yeonho.dev@gmail.com>
2020-09-11 00:04:01 -0400

    show an image by using pillow and save it

--- file_handling.py
+++ file_handling.py
@@ -1,5 +1,12 @@
-# Getting an image file by using request
+# Get an image file by using request
 import requests
 
 url = 'http://bit.ly/2JnsHnT'
 r = requests.get(url, stream=True).raw
+
+# Show an image by using pillow
+from PIL import Image
+
+img = Image.open(r)
+img.show()
+img.save('src.png')
