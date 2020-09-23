00d2fab01cfc0524464db6ada190adcbadd4207d
yeonhodev <yeonho.dev@gmail.com>
2020-09-22 23:36:36 -0400

    dataframe creation

--- pandas_dataframe.py
+++ pandas_dataframe.py
@@ -13,3 +13,23 @@ print(df.describe())
 
 # 데이터프레임의 인덱스 정보, 칼럼 정보, 메모리 사용량 등을 확인
 print(df.info())
+
+# 시리즈를 이요한 데이터프레임 생성
+kospi = pd.Series([1915, 1961, 2026, 2467, 2041], index=[2014, 2015, 2016, 2017, 2018], name='KOSPI')
+print(kospi)
+kosdaq = pd.Series([542, 682, 631, 798, 675], index=[2014, 2015, 2016, 2017, 2018], name='KOSDAQ')
+print(kosdaq)
+df = pd.DataFrame({kospi.name: kospi, kosdaq.name: kosdaq})
+print(df)
+
+# 리스트를 이용한 데이터프레임 생성
+columns = ['KOSPI', 'KOSDAQ']
+index = [2014, 2015, 2016, 2017, 2018]
+rows = []
+rows.append([1915, 542])
+rows.append([1961, 682])
+rows.append([2026, 631])
+rows.append([2467, 798])
+rows.append([2041, 675])
+df = pd.DataFrame(rows, columns=columns, index=index)
+print(df)
