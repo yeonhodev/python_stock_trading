cf29d3e48e186daf2e9e193ffa99c3c9e0938b5b
yeonhodev <yeonho.dev@gmail.com>
2020-09-24 00:07:43 -0400

    downloaded daily stock data for Samsung Electronics and Microsoft - used yfinance and pandas_datareader

---
+++ stock_comparison.py
@@ -0,0 +1,12 @@
+# source: github.com/ranaroussi/yfinance
+# get_data_yahoo(조회할 주식 종목 [, start=조회 기간의 시작일] [, end=조회 기간의 종료일])
+
+# 삼성전자(005930.KS)와 마이크로소프트(MSFT)의 일별 주가 데이터를 다운받아 어떤 종복의 수익률이 더 좋았는지 확인한다
+from pandas_datareader import data as pdr
+import yfinance as yf
+yf.pdr_override() # pdr_override() 함수와 pandas_datareader.data.get_data_yahoo() 함수를 사용하여 빠르게 데이터를 다운로드 할 수 있다.
+
+sec = pdr.get_data_yahoo('005930.KS', start='2018-05-04') 
+msft = pdr.get_data_yahoo('MSFT', start='2018-05-04')
+
+print(sec.head(10))
