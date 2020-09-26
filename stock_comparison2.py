9b5a3219a3231daa52f224fb035241eede3615ec
yeonhodev <yeonho.dev@gmail.com>
2020-09-25 23:58:53 -0400

    show graph

--- stock_comparison2.py
+++ stock_comparison2.py
@@ -1,3 +1,14 @@
 # 그래프 출력
 # plot(x, y, 마커 형태 [, label='Label'])
-from pandas_datareader
+from pandas_datareader import data as pdr
+import yfinance as yf
+yf.pdr_override()
+sec = pdr.get_data_yahoo('005930.KS', start='2018-05-04')
+msft = pdr.get_data_yahoo('MSFT', start='2018-05-04')
+
+import matplotlib.pyplot as plt
+
+plt.plot(sec.index, sec.Close, 'b', label='Samsung Electronics')
+plt.plot(msft.index, msft.Close, 'r--', label="Microsoft")
+plt.legend(loc='best')
+plt.show()
