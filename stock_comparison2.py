3a43164595e253b9977c39fd0689269f859ef5b1
yeonhodev <yeonho.dev@gmail.com>
2020-09-27 00:37:05 -0400

    histogram, normally discributed; leptokurtic distribution and fat tail

--- stock_comparison2.py
+++ stock_comparison2.py
@@ -36,3 +36,16 @@ print(sec_dpc.head())
 msft_dpc = (msft['Close'] / msft['Close'].shift(1) - 1) * 100
 msft_dpc.iloc[0] = 0
 print(msft_dpc.head())
+
+# Histogram은 frequency distribution을 나타내는 그래프로서, 데이터 값 들에 대한 구간별 빈도수를 막대 형태로 나타낸다. 이 때 구간 수를 bins라고 하는데 hist() 함수에서 사용되는 bins의 기본값은 10이다. 빈스에 따라 그래프 모양이 달라지므로 관측한 데이터 특성을 잘 보여주도록 빈스값을 정해야 한다. 삼성전자 주식 종가의 일간 변동률을 히스토그램으로 출력해보자. 맷플롯립에서 히스토그램은 hist() 함수를 사용한다. 삼성전자의 일간 변동률을 18개 구간으로 나누어 빈도수를 표시한다. 
+
+import matplotlib.pyplot as plt
+sec_dpc = (sec['Close'] / sec['Close'].shift(1) - 1) * 100
+sec_dpc.iloc[0] = 0
+plt.hist(sec_dpc, bins=18)
+plt.grid(True)
+plt.show()
+# 출력된 결과를 보면 삼성전자 일간 변동률 분포가 0 bin을 기준으로 좌우 대칭적이다. 정규분포 형태와 비슷하다. 엄밀히 얘기하자면, 주가 수익률은 정규분포보다 중앙 부분이 더 뾰족하고, 양쪽 꼬리는 더 두터운 것으로 알려져 있다. 이를 각각 급첨분포(leptokurtic distribution)와 팻 테일(fat tail)이라 부른다. 
+
+# 주가 수익률이 급첨분포를 나타낸다는 것은 정규분포와 비교했을 때 주가의 움직임이 대부분 매우 작은 범위 안에서 발생한다는 것을 의미한다. 그리고 두꺼운 꼬리를 가리키는 팻 테일은 그래프가 좌우 극단 부분에 해당하는 아주 큰 가격 변동이 정규분포보다 더 많이 발생한다는 의미다. 시리즈의 describe() 매서드를 이용하면 평균과 표준편차를 확인할 수 있다. 
+print(sec_dpc.describe())
