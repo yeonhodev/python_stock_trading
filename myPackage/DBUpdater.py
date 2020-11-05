96acce6518aa2745c501fd2b2248ee828b8e4d1b
yeonho.dev <yeonho.dev@gmail.com>
2020-11-04 23:25:34 -0500

    modified myPackage/DBUpdater.py

--- myPackage/DBUpdater.py
+++ myPackage/DBUpdater.py
@@ -1,9 +1,44 @@
+import pymysql
+import mariadb_config
+
+passwd = mariadb_config.passwd
+
 class DBUpdater:
     def __init__(self):
         """생성자: MariaDB 연결 및 종목코드 딕셔너리 생성"""
+        self.conn = pymysql.connect(host='localhost', user='root', password=passwd, db='INVESTAR', charset='utf8')
+
+        with self.conn.cursor() as curs:
+            sql = """
+            CREATE TABLE IF NOT EXISTS company_info (
+                code VARCHAR(20),
+                company VARCHAR(40),
+                last_update DATE, 
+                PRIMARY KEY (code)
+            )
+            """
+            curs.execute(sql)
+            sql = """
+            CREATE TABLE IF NOT EXISTS daily_price(
+                code VARCHAR(20), 
+                date DATE, 
+                open BIGINT(20),
+                high BIGINT(20),
+                low BIGINT(20), 
+                close BIGINT(20),
+                diff BIGINT(20),
+                volume BIGINT(20),
+                PRIMARY KEY (code, date)
+            )
+            """
+            curs.execute(sql)
+        self.conn.commit()
+
+        self.codes = dict()
 
     def __del__(self):
         """소멸자: MariaDB 연결 해제"""
+        self.conn.close()
     
     def read_krx_code(self):
         """KRX로 부터 상장법인목록 파일을 읽어와서 데이터프레임으로 변환"""
