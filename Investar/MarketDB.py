5353a4fee7d920a8c361b409d049dc866709f42f
yeonho.dev <yeonho.dev@gmail.com>
2020-11-16 00:10:30 -0500

    modified methods

--- Investar/MarketDB.py
+++ Investar/MarketDB.py
@@ -16,22 +16,22 @@ class MarketDB:
         # dict()로 생성 가능하지만, 조금 더 파이썬답게 생성하고자 리터럴을 사용하여 생성했다. 
         self.codes = {}
         # get_com_info() 함수를 호출하여 마리아디비에서 company_info 테이블을 읽어와서 codes에 저장한다.
-        self.get_com_info()
+        self.get_comp_info()
         
     def __del__(self):
         """소멸자: MariaDB 연결 해제"""
         self.conn.close()
 
-    def getCompanyInfo(self):
+    def get_comp_info(self):
         """company_info 테이블에서 읽어와서 companyData와 codes에 저장"""
         sql = "SELECT * FROM company_info"
         companyInfo = pd.read_sql(sql, self.conn)
         for idx in range(len(companyInfo)):
             self.codes[companyInfo['code'].values[idx]] = companyInfo['company'].values[idx]
 
-    def getDailyPrice(self, code, startDate, endDate):
+    def get_daily_price(self, code, start_date=None, end_date=None):
         """daily_price 테이블에서 읽어와서 데이터프레임으로 반환"""
-        sql = "SELECT * FROM daily_price WHERE code = '{}' and date >= '{}' and date <= '{}'".format(code, startDate, endDate)
+        sql = f"SELECT * FROM daily_price WHERE code = '{code}' and date >= '{start_date}' and date <= '{end_date}'"
         df = pd.read_sql(sql, self.conn)
         df.index = df['date']
         return df
