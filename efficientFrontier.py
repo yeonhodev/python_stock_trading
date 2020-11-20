643cf83be920358ec14ea6b099042ee62d7bef42
yeonho.dev <yeonho.dev@gmail.com>
2020-11-19 23:56:04 -0500

    modified efficientFrontier.py

---
+++ efficientFrontier.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as import pd
+import matplotlib.pyplot as plt
+from Investar import Analyzer
+
+mk = Analyzer.MarketDB()
+stocks = ['삼성전자', 'SK하이닉스', '현대자동차', 'NAVER']
+df = pd.DataFrame()
+for s in stocks:
+    df[s] = mk.get_daily_price(s, '2016-01-04', '2018-04-27')['close']
