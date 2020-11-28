eeb0ce3e39b5b057f2ccd9dfacb07a94b104707e
yeonho.dev <yeonho.dev@gmail.com>
2020-11-27 23:51:38 -0500

    Added codes for %b

--- myPackage/BollingerBand.py
+++ myPackage/BollingerBand.py
@@ -12,10 +12,14 @@ df['stddev'] = df['close'].rolling(window=20).std()
 df['upper'] = df['MA20'] + (df['stddev'] * 2)
 # 중간 볼린저 밴드 - (2 x 표준편차)를 하단 볼린저 밴드로 계산한다. 
 df['lower'] = df['MA20'] - (df['stddev'] * 2)
+# (종가 - 하단 밴드) / (상단 밴드 - 하단 밴드)를 구해 %B 칼럼을 생성한다.
+df['PB'] = (df['close'] - df['lower']) / (df['upper'] - df['lower'])
 # 위는 19번째 행까지 NaN 이므로 값이 있는 20번째 행부터 사용한다.
 df = df[19:]
 
-plt.figure(figsize=(9, 5))
+plt.figure(figsize=(9, 8))
+# 기존의 볼린저 밴드 차트를 2행 1열의 그리드에서 1열에 배치한다. 
+plt.subplot(2, 1, 1)
 # x좌표 df.index에 해당하는 종가를 y좌표로 설정해 파란색(#0000ff) 실선으로 표시한다.
 plt.plot(df.index, df['close'], color='#0000ff', label='Close')
 # x좌표 df.index에 해당하는 상단 볼린저 밴드값을 y좌표로 설정해 붉은 실선(r--)으로 표시한다.
@@ -24,6 +28,13 @@ plt.plot(df.index, df['MA20'], 'k--', label='Moving average 20')
 plt.plot(df.index, df['lower'], 'c--', label='Lower band')
 # 상단 볼린저 밴드와 하단 볼린저 밴드 사이를 회색으로 칠한다.
 plt.fill_between(df.index, df['upper'], df['lower'], color='0.9')
-plt.legend(loc='best')
 plt.title('NAVER Bollinger Band (20 day, 2 std)')
+plt.legend(loc='best')
+
+# %B 차트를 2행 1열의 그리드에서 2열에 배치한다.
+plt.subplot(2, 1, 2)
+# x좌표 df.index 에 해당하는 %b 값을 y좌표로 설정해 파란(b)실선으로 표시한다. 
+plt.plot(df.index, df['PB'], color='b', label='%B')
+plt.grid(True)
+plt.legend(loc='best')
 plt.show()
