70983a2a4bdd6eb7aacabd02c7ef828a848de3f6
yeonho.dev <yeonho.dev@gmail.com>
2021-01-03 20:03:55 -0500

    Modified EtfAlgoTrader.py

--- myPackage/EtfAlgoTrader.py
+++ myPackage/EtfAlgoTrader.py
@@ -36,3 +36,16 @@ def dbgout(message):
     strbuf = datetime.now().strftime('[%m/%d %H:%M:%S]') + message
     # etf-algo-trading 채널로 메세지를 보내려면 워크스페이스에 etf-algo-trading 채널을 미리 만들어 둬야 한다. 별도의 채널을 만들기 싫다면 #etf-algo-trading 대신 #general을 인수로 주어 일반 채널로 메시지를 보내도 된다. 
     slack.chat.post_message('#etf-algo-trading', strbuf)
+
+cpStock = win32com.client.Dispatch("DsCbo1.StockMst") # 주식 종목별 정보
+
+def get_current_price(code):
+    cpStock.SetInputValue(0, code) # 종목코드에 대한 가격 정보
+    cpStock.BlockRequest()
+
+    item = {}
+    item['cur_price'] = cpStock.GetHeaderValue(11) # 현재가
+    item['ask'] = cpStock.GetHeaderValue(16) # 매수호가
+    item['bid'] = cpStock.GetHeaderValue(17) # 매도호가
+
+    return item['cur_price'], item['ask'], item['bid']
